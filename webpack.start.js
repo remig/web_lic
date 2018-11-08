3e500af118d22f887b1253f24035bdd895d508bd
Remi Gagne <remigagne@gmail.com>
2018-11-07 16:45:10 -0800

    Add script to run webpack's dev server.  Ignore ldraw files, which may be deployed inside the project for easier dev server setup.

---
+++ webpack.start.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const {VueLoaderPlugin} = require('vue-loader');
+
+module.exports = {
+	entry: './src/ui.js',
+	output: {
+		filename: 'bundle.js',
+		chunkFilename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: 'dist/'
+	},
+	mode: 'development',
+	devtool: 'source-map',
+	devServer: {
+		contentBase: './'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+				options: {
+					hotReload: false
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'vue-style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.(js|vue)$/,
+				exclude: [/node_modules/, /dialog\.js/],
+				loader: 'eslint-loader',
+				options: {
+					failOnWarning: false,
+					failOnError:  true
+				}
+			}
+		]
+	},
+	plugins: [
+		new VueLoaderPlugin()
+	]
+};
