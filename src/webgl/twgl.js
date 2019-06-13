d4db21665404202fe6d07cfb09a47ec26210274b
Remi <remigagne@gmail.com>
2019-06-12 17:12:41 -0700

    Remove more unused gl helper code

--- src/webgl/twgl.js
+++ src/webgl/twgl.js
@@ -5,61 +5,11 @@
  */
 /* eslint-disable */
 
-var error = (window.console && window.console.error && typeof window.console.error === "function")
-  ? window.console.error.bind(window.console)
-  : function() { };
-
 var twgl = (function() {
   
-  var FLOAT                         = 0x1406;
-  var FLOAT_VEC2                    = 0x8B50;
-  var FLOAT_VEC3                    = 0x8B51;
-  var FLOAT_VEC4                    = 0x8B52;
-  var INT                           = 0x1404;
-  var INT_VEC2                      = 0x8B53;
-  var INT_VEC3                      = 0x8B54;
-  var INT_VEC4                      = 0x8B55;
-  var BOOL                          = 0x8B56;
-  var BOOL_VEC2                     = 0x8B57;
-  var BOOL_VEC3                     = 0x8B58;
-  var BOOL_VEC4                     = 0x8B59;
-  var FLOAT_MAT2                    = 0x8B5A;
-  var FLOAT_MAT3                    = 0x8B5B;
-  var FLOAT_MAT4                    = 0x8B5C;
-  var FLOAT_MAT2x3                  = 0x8B65;
-  var FLOAT_MAT2x4                  = 0x8B66;
-  var FLOAT_MAT3x2                  = 0x8B67;
-  var FLOAT_MAT3x4                  = 0x8B68;
-  var FLOAT_MAT4x2                  = 0x8B69;
-  var FLOAT_MAT4x3                  = 0x8B6A;
-  var UNSIGNED_INT                  = 0x1405;
-  var UNSIGNED_INT_VEC2             = 0x8DC6;
-  var UNSIGNED_INT_VEC3             = 0x8DC7;
-  var UNSIGNED_INT_VEC4             = 0x8DC8;
-
-  var typeMap = {};
-
   function floatSetter(gl, location) {
     return function(v) {
       gl.uniform1f(location, v);
-    };
-  }
-
-  function floatArraySetter(gl, location) {
-    return function(v) {
-      gl.uniform1fv(location, v);
-    };
-  }
-
-  function floatVec2Setter(gl, location) {
-    return function(v) {
-      gl.uniform2fv(location, v);
-    };
-  }
-
-  function floatVec3Setter(gl, location) {
-    return function(v) {
-      gl.uniform3fv(location, v);
     };
   }
 
@@ -69,199 +19,42 @@ var twgl = (function() {
     };
   }
 
-  function intSetter(gl, location) {
-    return function(v) {
-      gl.uniform1i(location, v);
-    };
-  }
-
-  function intArraySetter(gl, location) {
-    return function(v) {
-      gl.uniform1iv(location, v);
-    };
-  }
-
-  function intVec2Setter(gl, location) {
-    return function(v) {
-      gl.uniform2iv(location, v);
-    };
-  }
-
-  function intVec3Setter(gl, location) {
-    return function(v) {
-      gl.uniform3iv(location, v);
-    };
-  }
-
-  function intVec4Setter(gl, location) {
-    return function(v) {
-      gl.uniform4iv(location, v);
-    };
-  }
-
-  function uintSetter(gl, location) {
-    return function(v) {
-      gl.uniform1ui(location, v);
-    };
-  }
-
-  function uintArraySetter(gl, location) {
-    return function(v) {
-      gl.uniform1uiv(location, v);
-    };
-  }
-
-  function uintVec2Setter(gl, location) {
-    return function(v) {
-      gl.uniform2uiv(location, v);
-    };
-  }
-
-  function uintVec3Setter(gl, location) {
-    return function(v) {
-      gl.uniform3uiv(location, v);
-    };
-  }
-
-  function uintVec4Setter(gl, location) {
-    return function(v) {
-      gl.uniform4uiv(location, v);
-    };
-  }
-
-  function floatMat2Setter(gl, location) {
-    return function(v) {
-      gl.uniformMatrix2fv(location, false, v);
-    };
-  }
-
-  function floatMat3Setter(gl, location) {
-    return function(v) {
-      gl.uniformMatrix3fv(location, false, v);
-    };
-  }
-
   function floatMat4Setter(gl, location) {
     return function(v) {
       gl.uniformMatrix4fv(location, false, v);
     };
   }
 
-  function floatMat23Setter(gl, location) {
-    return function(v) {
-      gl.uniformMatrix2x3fv(location, false, v);
-    };
+  var FLOAT      = 0x1406;
+  var FLOAT_VEC4 = 0x8B52;
+  var FLOAT_MAT4 = 0x8B5C;
+
+  var typeMap = {};
+  typeMap[FLOAT] = floatSetter;
+  typeMap[FLOAT_VEC4] = floatVec4Setter;
+  typeMap[FLOAT_MAT4] = floatMat4Setter;
+
+  function createUniformSetter(gl, program, uniformInfo) {
+	var location = gl.getUniformLocation(program, uniformInfo.name);
+	var isArray = (uniformInfo.size > 1 && uniformInfo.name.substr(-3) === "[0]");
+	return typeMap[uniformInfo.type](gl, location);
   }
-
-  function floatMat32Setter(gl, location) {
-    return function(v) {
-      gl.uniformMatrix3x2fv(location, false, v);
-    };
-  }
-
-  function floatMat24Setter(gl, location) {
-    return function(v) {
-      gl.uniformMatrix2x4fv(location, false, v);
-    };
-  }
-
-  function floatMat42Setter(gl, location) {
-    return function(v) {
-      gl.uniformMatrix4x2fv(location, false, v);
-    };
-  }
-
-  function floatMat34Setter(gl, location) {
-    return function(v) {
-      gl.uniformMatrix3x4fv(location, false, v);
-    };
-  }
-
-  function floatMat43Setter(gl, location) {
-    return function(v) {
-      gl.uniformMatrix4x3fv(location, false, v);
-    };
-  }
-
-  typeMap[FLOAT]             = { Type: Float32Array, size:  4, setter: floatSetter,      arraySetter: floatArraySetter};
-  typeMap[FLOAT_VEC2]        = { Type: Float32Array, size:  8, setter: floatVec2Setter,  };
-  typeMap[FLOAT_VEC3]        = { Type: Float32Array, size: 12, setter: floatVec3Setter,  };
-  typeMap[FLOAT_VEC4]        = { Type: Float32Array, size: 16, setter: floatVec4Setter,  };
-  typeMap[INT]               = { Type: Int32Array,   size:  4, setter: intSetter,        arraySetter: intArraySetter};
-  typeMap[INT_VEC2]          = { Type: Int32Array,   size:  8, setter: intVec2Setter,    };
-  typeMap[INT_VEC3]          = { Type: Int32Array,   size: 12, setter: intVec3Setter,    };
-  typeMap[INT_VEC4]          = { Type: Int32Array,   size: 16, setter: intVec4Setter,    };
-  typeMap[UNSIGNED_INT]      = { Type: Uint32Array,  size:  4, setter: uintSetter,       arraySetter: uintArraySetter};
-  typeMap[UNSIGNED_INT_VEC2] = { Type: Uint32Array,  size:  8, setter: uintVec2Setter,   };
-  typeMap[UNSIGNED_INT_VEC3] = { Type: Uint32Array,  size: 12, setter: uintVec3Setter,   };
-  typeMap[UNSIGNED_INT_VEC4] = { Type: Uint32Array,  size: 16, setter: uintVec4Setter,   };
-  typeMap[BOOL]              = { Type: Uint32Array,  size:  4, setter: intSetter,        arraySetter: intArraySetter};
-  typeMap[BOOL_VEC2]         = { Type: Uint32Array,  size:  8, setter: intVec2Setter,    };
-  typeMap[BOOL_VEC3]         = { Type: Uint32Array,  size: 12, setter: intVec3Setter,    };
-  typeMap[BOOL_VEC4]         = { Type: Uint32Array,  size: 16, setter: intVec4Setter,    };
-  typeMap[FLOAT_MAT2]        = { Type: Float32Array, size: 16, setter: floatMat2Setter,  };
-  typeMap[FLOAT_MAT3]        = { Type: Float32Array, size: 36, setter: floatMat3Setter,  };
-  typeMap[FLOAT_MAT4]        = { Type: Float32Array, size: 64, setter: floatMat4Setter,  };
-  typeMap[FLOAT_MAT2x3]      = { Type: Float32Array, size: 24, setter: floatMat23Setter, };
-  typeMap[FLOAT_MAT2x4]      = { Type: Float32Array, size: 32, setter: floatMat24Setter, };
-  typeMap[FLOAT_MAT3x2]      = { Type: Float32Array, size: 24, setter: floatMat32Setter, };
-  typeMap[FLOAT_MAT3x4]      = { Type: Float32Array, size: 48, setter: floatMat34Setter, };
-  typeMap[FLOAT_MAT4x2]      = { Type: Float32Array, size: 32, setter: floatMat42Setter, };
-  typeMap[FLOAT_MAT4x3]      = { Type: Float32Array, size: 48, setter: floatMat43Setter, };
-
-  var gl = undefined;
 
   function createUniformSetters(gl, program) {
 
-    function createUniformSetter(program, uniformInfo) {
-      var location = gl.getUniformLocation(program, uniformInfo.name);
-      var isArray = (uniformInfo.size > 1 && uniformInfo.name.substr(-3) === "[0]");
-      var type = uniformInfo.type;
-      var typeInfo = typeMap[type];
-      if (typeInfo.arraySetter && isArray) {
-        return typeInfo.arraySetter(gl, location);
-      } else {
-        return typeInfo.setter(gl, location);
-      }
-    }
-
-    var uniformSetters = { };
+    var uniformSetters = {};
     var numUniforms = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS);
 
-    for (var ii = 0; ii < numUniforms; ++ii) {
-      var uniformInfo = gl.getActiveUniform(program, ii);
-      if (!uniformInfo) {
-        break;
-      }
-      var name = uniformInfo.name;
-      // remove the array suffix.
-      if (name.substr(-3) === "[0]") {
-        name = name.substr(0, name.length - 3);
-      }
-      var setter = createUniformSetter(program, uniformInfo);
-      uniformSetters[name] = setter;
+    for (var i = 0; i < numUniforms; ++i) {
+      var uniformInfo = gl.getActiveUniform(program, i);
+      uniformSetters[uniformInfo.name] = createUniformSetter(gl, program, uniformInfo);
     }
     return uniformSetters;
   }
 
   function setUniforms(setters, values) {
-    var actualSetters = setters.uniformSetters || setters;
-    var numArgs = arguments.length;
-    for (var andx = 1; andx < numArgs; ++andx) {
-      var vals = arguments[andx];
-      if (Array.isArray(vals)) {
-        var numValues = vals.length;
-        for (var ii = 0; ii < numValues; ++ii) {
-          setUniforms(actualSetters, vals[ii]);
-        }
-      } else {
-        for (var name in vals) {
-          var setter = actualSetters[name];
-          if (setter) {
-            setter(vals[name]);
-          }
-        }
-      }
+    for (var name in values) {
+      setters.uniformSetters[name](values[name]);
     }
   }
 
