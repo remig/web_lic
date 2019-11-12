cb2082a396526c03015cc50f6f88722a76f0c7bf
Remi <remigagne@gmail.com>
2019-11-12 00:21:01 -0800

    Start typing the basic store items

---
+++ src/item_types.ts
@@ -0,0 +1,43 @@
+export interface Point {
+	x: number;
+	y: number;
+}
+
+export interface Box extends Point {
+	width: number;
+	height: number;
+}
+
+export interface Item {
+	id: number;
+	type: string;
+	parent: any;
+}
+
+export type PageLayouts = 'horizontal' | 'vertical'
+export type PageSubtypes = 'templatePage' | 'page' | 'titlePage' | 'inventoryPage'
+
+export interface Page extends Item {
+	type: 'page';
+	subtype: PageSubtypes;
+	steps: any[];
+	dividers: any[];
+	annotations: any[];
+	pliItems: any[];
+	needsLayout: boolean;
+	locked: boolean;
+	stretchedStep: any;
+	innerContentOffset: Point;
+	number: number;
+	numberLabelID: number;
+	layout: PageLayouts
+}
+
+export interface PLIItem extends Item, Box {
+	type: 'pliItem';
+	domID: string | null;
+	filename: string;
+	colorCode: number;
+	quantity: number;
+	quantityLabelID: number;
+}
