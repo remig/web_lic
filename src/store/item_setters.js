12445e76ee419a93c98088108c58419b1d819cc9
Remi <remigagne@gmail.com>
2019-11-12 23:23:03 -0800

    More opts destructuring

--- src/store/item_setters.js
+++ src/store/item_setters.js
@@ -5,27 +5,31 @@ import store from '../store';
 import Layout from '../layout';
 
 const api = {
-	add(opts) {  // opts: {itemJSON, parent, insertionIndex = -1, parentInsertionIndex = =1}
-		const item = opts.item;
+	add({
+		item,
+		parent,
+		insertionIndex = -1,
+		parentInsertionIndex = -1,
+	}) {
 		item.id = store.get.nextItemID(item);
 		if (store.state.hasOwnProperty(item.type)) {
 			store.state[item.type] = item;
 		} else {
-			_.insert(store.state[item.type + 's'], item, opts.insertionIndex);
+			_.insert(store.state[item.type + 's'], item, insertionIndex);
 		}
-		if (opts.parent) {
-			const parent = store.get.lookupToItem(opts.parent);
+		if (parent) {
+			parent = store.get.lookupToItem(parent);
 			item.parent = {type: parent.type, id: parent.id};
 			if (parent.hasOwnProperty(item.type + 's')) {
-				_.insert(parent[item.type + 's'], item.id, opts.parentInsertionIndex);
+				_.insert(parent[item.type + 's'], item.id, parentInsertionIndex);
 			} else if (parent.hasOwnProperty(item.type + 'ID')) {
 				parent[item.type + 'ID'] = item.id;
 			}
 		}
 		return item;
 	},
-	delete(opts) {  // opts: {item}
-		const item = store.get.lookupToItem(opts.item);
+	delete({item}) {
+		item = store.get.lookupToItem(item);
 		_.deleteItem(store.state[item.type + 's'], item);
 		if (item.parent) {
 			const parent = store.get.lookupToItem(item.parent);
@@ -36,20 +40,20 @@ const api = {
 			}
 		}
 	},
-	deleteChildList(opts) {  // opts: {item, listType}
-		const itemType = store.mutations[opts.listType] ? opts.listType : 'item';
-		const parent = store.get.lookupToItem(opts.item);
-		const list = parent[opts.listType + 's'] || [];
+	deleteChildList({item, listType}) {
+		const itemType = store.mutations[listType] ? listType : 'item';
+		const parent = store.get.lookupToItem(item);
+		const list = parent[listType + 's'] || [];
 		while (list.length) {
 			store.mutations[itemType].delete({
-				[itemType]: {type: opts.listType, id: list[0]},
+				[itemType]: {type: listType, id: list[0]},
 			});
 		}
 	},
-	reparent(opts) {  // opts: {item, newParent, parentInsertionIndex = -1}
-		const item = store.get.lookupToItem(opts.item);
+	reparent({item, newParent, parentInsertionIndex = -1}) {
+		item = store.get.lookupToItem(item);
 		const oldParent = store.get.parent(item) || {};
-		const newParent = store.get.lookupToItem(opts.newParent);
+		newParent = store.get.lookupToItem(newParent);
 		item.parent.id = newParent.id;
 		item.parent.type = newParent.type;
 		if (oldParent.hasOwnProperty(item.type + 's')) {
@@ -58,29 +62,29 @@ const api = {
 			oldParent[item.type + 'ID'] = null;
 		}
 		if (newParent.hasOwnProperty(item.type + 's')) {
-			_.insert(newParent[item.type + 's'], item.id, opts.parentInsertionIndex);
+			_.insert(newParent[item.type + 's'], item.id, parentInsertionIndex);
 		} else if (newParent.hasOwnProperty(item.type + 'ID')) {
 			newParent[item.type + 'ID'] = item.id;
 		}
 	},
-	reposition(opts) {  // opts: {item or [items], dx, dy}
-		const items = Array.isArray(opts.item) ? opts.item : [opts.item];
-		items.forEach(item => {
-			if (item.type === 'divider') {  // special case: dividers must move both points
+	reposition({item, dx, dy}) {
+		const items = Array.isArray(item) ? item : [item];
+		items.forEach(localItem => {
+			if (localItem.type === 'divider') {  // special case: dividers must move both points
 				// TODO: change 'p1' & 'p2' in divider to proper points; won't need this special case
-				store.mutations.divider.reposition(opts);
-			} else if (item.points) {
-				item.points.forEach(pt => {
+				store.mutations.divider.reposition({item: localItem, dx, dy});
+			} else if (localItem.points) {
+				localItem.points.forEach(pt => {
 					pt = store.get.point(pt);
-					pt.x += opts.dx;
-					pt.y += opts.dy;
+					pt.x += dx;
+					pt.y += dy;
 				});
 			} else {
-				item.x += opts.dx;
-				item.y += opts.dy;
-				const parentType = item.parent.type;
+				localItem.x += dx;
+				localItem.y += dy;
+				const parentType = localItem.parent.type;
 				if (Layout.adjustBoundingBox[parentType]) {
-					Layout.adjustBoundingBox[parentType](item.parent);
+					Layout.adjustBoundingBox[parentType](localItem.parent);
 				}
 			}
 		});
